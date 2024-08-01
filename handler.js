191dcd88e2f154357175936ec9b3f579443e066b
pretzel <132411345+presigues@users.noreply.github.com>
2024-07-31 22:17:23 -0400

    update

--- handler.js
+++ handler.js
@@ -11,55 +11,25 @@ const game3="https://www.youtube.com/embed/VqOKzoHJDjA";  // strong
 const game4="https://www.youtube.com/embed/EMiNmJW7enI?list=PLu4m0YMKEfPqmA4Di8Y9XkPannXdOZn8u"; // steamworks
 const game5="https://www.youtube.com/embed/HZbdwYiCY74?list=PLu4m0YMKEfPqmA4Di8Y9XkPannXdOZn8u"; // power up
 const game6="https://www.youtube.com/embed/Mew6G_og-PI?list=PLu4m0YMKEfPqmA4Di8Y9XkPannXdOZn8u"; // deep
-const game7="";
-const game8="";
-const game9="";
-const game10="";
+const game7="https://www.youtube.com/embed/gmiYWTmFRVE?list=PLu4m0YMKEfPqmA4Di8Y9XkPannXdOZn8u"; // infinite
+const game8="https://www.youtube.com/embed/itHNW2OFr4Y?list=PLu4m0YMKEfPqmA4Di8Y9XkPannXdOZn8u"; // ultimate ascent
+const game9="https://www.youtube.com/embed/ZnGfbGzEFrM?list=PLu4m0YMKEfPqmA4Di8Y9XkPannXdOZn8u"; // lunacy
+const game10="https://www.youtube.com/embed/uYNu9cQac1o?list=PLu4m0YMKEfPqmA4Di8Y9XkPannXdOZn8u"; // stack attack
 
 
-function thehardpart(){
+function hard(){
+round = 1;
 
- round1 = getRandomInt(1, 10);
- round2 = getRandomInt(1, 10);
-while(round1 == round2){
-    round2 = getRandomInt(1, 10);
+if(round == 1){
+    document.getElementById("game1").setAttribute("src", game1);
+            round = 2;
 }
- round3 = getRandomInt(1, 10);
-while(round1 == round3 || round2 == round3){
-    round3 = getRandomInt(1, 10);
-}
- round4 = getRandomInt(1, 10);
-while(round1==round4||round2==round4||round3==round4){
-    round4 == getRandomInt(1,10);
-}
- round5 = getRandomInt(1, 10);
-while(round1==round5||round2==round5||round3==round5||round4==round5){
-     round5 = getRandomInt(1, 10);
-}
- round6 = getRandomInt(1, 10);
-while(round1==round6||round2==round6||round3==round6||round4==round6||round5==round6){
-    round6 = getRandomInt(1, 10);
-}
- round7 = getRandomInt(1, 10);
-while(round1==round7||round2==round7||round3==round7||round4==round7||round5==round7||round6==round7){
-    round7 = getRandomInt(1, 10);
-}
- round8 = getRandomInt(1, 10);
-while(round1==round8||round2==round8||round3==round8||round4==round8||round5==round8||round6==round8||round7==round8){
-    round8 = getRandomInt(1, 10);
-}
- round9 = getRandomInt(1, 10);
-while(round1==round9||round2==round9||round3==round9||round4==round9||round5==round9||round6==round9||round7==round9||round8==round9){
-    round9 = getRandomInt(1, 10);
-}
- round10 = getRandomInt(1, 10);
-while(round1==round10||round2==round10||round3==round10||round4==round10||round5==round10||round6==round10||round7==round10||round8==round10||round9==round10){
-    round10 = getRandomInt(1, 10);
+
 }
 
 
 
-}
+
 
 
   
